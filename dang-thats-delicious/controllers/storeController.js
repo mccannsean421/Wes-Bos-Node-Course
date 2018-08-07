08ee7c0409a139571b3a0969065719ec4fbfac17
C Mccann <mccannsean421@gmail.com>
2018-08-07 19:48:28 +0100

    Finished video 8: middleware, finished module 2

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = "Cormac";
+    if(req.name === "Cormac") {
+        throw Error('that is a stupid name');
+    }
+    next();
+}
+
 exports.homePage = (req, res) => {
+    console.log(req.name);
+    res.cookie('name', 'cormac', {maxAge: 90000});
     res.render('index');
 }
